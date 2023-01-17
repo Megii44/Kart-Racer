1d625fa11494425e51bd78eb4ab31e0185bfc1e1
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-01-17 14:44:25 +0100

    Added Gem Spawner

---
+++ Unity/Assets/Scripts/CollectibleSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CollectibleSpawner : MonoBehaviour
+{
+    public GameObject collectiblePrefab; // Drag the collectible prefab here in the Unity editor
+    public float spawnInterval = 1f; // The time interval between spawning new collectibles
+    public Vector3 spawnArea; // The area in which collectibles can be spawned
+    public int count = 0;
+    private float spawnTimer;
+
+    void Start()
+    {
+        spawnTimer = 0f;
+    }
+
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnInterval && count < 100)
+        {
+            // Reset the spawn timer
+            spawnTimer = 0f;
+
+            for(int i = 0; i < 100; i++)
+            {
+                // Generate a random position within the spawn area
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnArea.x, spawnArea.x), 12f, Random.Range(-spawnArea.z, spawnArea.z + 700f));
+
+                // Instantiate the collectible prefab at the random position
+                Instantiate(collectiblePrefab, spawnPosition, Quaternion.identity);
+
+                count = count + 1;
+            }
+        }
+    }
+}
